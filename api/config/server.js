ce8bf80fc63dd0b6eb62af33eaffb7f74f46d584
Дарья 🥝 <daria@MacBook-Pro-Dara.local>
2022-10-03 17:49:04 +0700

    first commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5cdb2b77f34bd061b630ab566d518ef9'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '53b26f09-8b00-4cde-a8ad-5b87f967ae64'
+};
